5b4ee8444986b73737c5065ab980a42e6df5bcba
VascoUP <vasco_mag_pereira@hotmail.com>
2018-04-12 17:05:48 +0100

    Fix UI

---
+++ Vakuo/Assets/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenuController : MonoBehaviour {
+
+    public GameObject inventory;
+    public Button resumeButton;
+    public Button inventoryButton;
+    public Button exitButton;
+
+    void Start ()
+    {
+        Time.timeScale = 0f;
+        resumeButton.onClick.AddListener(() =>
+        {
+            Time.timeScale = 1f;
+            gameObject.SetActive(false);
+        });
+        inventoryButton.onClick.AddListener(() =>
+        {
+            gameObject.SetActive(false);
+            inventory.SetActive(true);
+        });
+        exitButton.onClick.AddListener(() =>
+        {
+        });
+
+    }
+	
+	void Update () {
+		
+	}
+}
