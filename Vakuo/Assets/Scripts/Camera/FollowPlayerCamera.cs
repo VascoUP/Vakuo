33eb5f64338cc8219835dba021f5f8388a855194
VascoUP <vasco_mag_pereira@hotmail.com>
2018-05-11 16:22:24 +0100

    Changed player movement

--- Vakuo/Assets/Scripts/Camera/FollowPlayerCamera.cs
+++ Vakuo/Assets/Scripts/Camera/FollowPlayerCamera.cs
@@ -54,7 +54,7 @@ public class FollowPlayerCamera : MonoBehaviour
         }
     }
 
-    IEnumerator Shake()
+    private IEnumerator Shake()
     {
         _isShaking = true;
 
---
+++ Vakuo/Assets/Scripts/Player/PlayerMeshController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMeshController : MonoBehaviour {
+    [SerializeField]
+    private GameObject _body;
+
+    [SerializeField]
+    private float _damping;
+
+    [SerializeField]
+    private float _maxInputError = 0.01f;
+
+    private float _localRotAngle = 0.0f;
+    private float _rotAngle = 0.0f;
+    private Vector3 _playerRotAxis;
+    private float _playerRotAngle = 0.0f;
+
+    private Quaternion RotToQuaternion()
+    {
+        _rotAngle = _localRotAngle + _playerRotAngle * _playerRotAxis.y;
+        return Quaternion.AngleAxis(_rotAngle, Vector3.up);
+    }
+
+    private void UpdateValues()
+    {
+        _localRotAngle = Mathf.Atan2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * Mathf.Rad2Deg;
+        transform.rotation.ToAngleAxis(out _playerRotAngle, out _playerRotAxis);
+    }
+    
+	private void LateUpdate ()
+    {
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) >= _maxInputError || Mathf.Abs(Input.GetAxisRaw("Vertical")) >= _maxInputError)
+        {
+            UpdateValues();
+        }
+
+        _body.transform.rotation = Quaternion.Lerp(_body.transform.rotation, RotToQuaternion(), Time.deltaTime * _damping);
+    }
+}
