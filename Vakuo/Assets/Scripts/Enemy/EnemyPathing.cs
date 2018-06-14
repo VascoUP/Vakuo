b4acf53aade805308e6b1ba900fc28bbfd6141b6
VascoUP <vasco_mag_pereira@hotmail.com>
2018-06-14 21:10:58 +0100

    Finalized mountain

---
+++ Vakuo/Assets/Scripts/Enemy/EnemyPathing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour {
+    [SerializeField]
+    private BezierCurve _defaultPath;
+
+    private BezierPathing _pathing;
+
+	private void OnEnable () {
+		if(_defaultPath != null)
+        {
+            _pathing = new BezierPathing();
+        } 
+	}
+	
+	private void Update () {
+		
+	}
+}
