23a6890bf7ae27fbcc2dd93400214014042a36f5
VascoUP <vasco_mag_pereira@hotmail.com>
2018-06-17 22:44:44 +0100

    Only draw plants at a certain distance

---
+++ Vakuo/Assets/InitializeCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializeCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Camera camera = GetComponent<Camera>();
+        float[] distances = new float[32];
+        distances[13] = 100;
+        camera.layerCullDistances = distances;
+	}
+}
